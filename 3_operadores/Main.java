6a4fb480ceb588170767f506d63553484506f690
“DaviT” <borbatrefs.davi@gmail.com>
2026-08-19 22:30:04 -0300

    coisas novas uau

---
+++ 3_operadores/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+public static void main(String[] args) {
+    
+int valor_1 = 10;
+int valor_2 = 20;
+int soma = 0;
+
+soma = valor_1 + valor_2;
+
+System.out.println("O valor 1 vai ta sendo: " + valor_1);
+System.out.println("E o valor 2 ta como: " + valor_2);
+System.out.println("Ai tu soma e da uns " + soma + " tlgd");
+
+
+}
+     
+}
---
+++ 4_strings/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args) {
+    String saudacao = "Hello World";
+    System.out.println(saudacao.length());
+    System.out.println(saudacao.toUpperCase());
+    System.out.println(saudacao.toLowerCase());
+}
+    
+}
